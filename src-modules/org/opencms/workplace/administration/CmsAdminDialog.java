b7a2eba32d9530a22aa85942d45e52c4a32718e7
Mobb autofixer <git@mobb.ai>
2023-09-12 22:15:43 +0000

    mobb fix commit: 7375a582-be89-40f4-a352-6d68bd6b04e7

--- src-modules/org/opencms/workplace/administration/CmsAdminDialog.java
+++ src-modules/org/opencms/workplace/administration/CmsAdminDialog.java
@@ -38,6 +38,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.PageContext;
+import org.springframework.web.util.HtmlUtils;
 
 /**
  * Workplace class for /system/workplace/views/admin/admin-main.jsp .<p>
